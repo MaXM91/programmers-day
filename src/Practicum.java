3bb57f73adaa9790d00b9e4dc4830fb7303fe8e7
MM91 <mmv_91@bk.ru>
2022-10-06 19:11:17 +0500

    Убрал лишние скобки

--- src/Practicum.java
+++ src/Practicum.java
@@ -7,23 +7,22 @@ public class Practicum {
         System.out.println("Введите год:");
         int year = scanner.nextInt();
 
-        if (isLeapYear(year)) {
+        if (isLeapYear(year))
             System.out.println("День программиста в этом году отмечается: 12.09."+ year);
-        }
         else
             System.out.println("День программиста в этом году отмечается: 13.09."+ year);
     }
 
     public static boolean isLeapYear(int year) {
 
-           if( (year % 400) == 0)
+           if( year % 400 == 0)
                return true;
-           else if ( (year % 100) == 0) {
+           else if (year % 100 == 0) {
                return false;
            }
-           else if ( (year % 4) == 0)
+           else if ( year % 4 == 0) {
                return true;
-           else
+           } else
                return false;
     }
 }
